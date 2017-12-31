cf797baec9e56af03f3cf3f82e26bc0f8fa0c2d1
Accelerator <13307130144@fudan.edu.cn>
2017-12-31 18:30:55 +0800

    add

---
+++ 1089.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+long long a,b;
+
+int main() {
+
+    while (cin >> a >> b) 
+        cout << a + b << endl;
+    
+    return 0;
+}
