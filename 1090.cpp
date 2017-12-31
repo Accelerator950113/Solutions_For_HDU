2f4493c7294ec0390098a27c15c073893f2fb56e
Accelerator <13307130144@fudan.edu.cn>
2017-12-31 18:37:56 +0800

    add

---
+++ 1090.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+long long a,b;
+int n;
+
+int main() {
+
+	cin >> n;
+    while (n --) { 
+    	cin >> a >> b;
+        cout << a + b << endl;
+    }
+    
+    return 0;
+}
